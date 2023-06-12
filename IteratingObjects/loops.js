1e60bc50fefa8bb5091b6c12b1e743204f5a88de
Kirti Panchal <panchalkirti2976@gmail.com>
2023-06-12 11:05:38 +0530

    Intro To for...of loop and for...in loop

--- IteratingObjects/loops.js
+++ IteratingObjects/loops.js
@@ -1,4 +1,4 @@
-let x = [10, 20, 30, 40, 50, 60];
+let arr = [10, 20, 30, 40, 50, 60];
 
 let obj1 = {
 	1: "kirti",
@@ -7,10 +7,36 @@ let obj1 = {
 	50: "panchal",
 };
 
-for (const key in x) {
+let parent = {
+	name: "xyz",
+	age: 50,
+};
+
+console.log("--- Printing Content From Array ---");
+for (const key in arr) {
+	console.log(`${key} ${typeof key}`);
+}
+console.log("\n");
+console.log("--- Printing Content From Object ---");
+for (const key in obj1) {
+	console.log(key);
+}
+console.log("\n");
+
+console.log(
+	"--- Printing Content From Object After Attaching Prototype Object ---"
+);
+obj1.__proto__ = parent;
+for (const key in obj1) {
 	console.log(key);
 }
 
 for (const key in obj1) {
-	console.log(key);
+	!Object.hasOwn(obj1, key) || console.log(key);
+}
+
+console.log("\n String iteration using for..of anf for..in");
+let name = "kirti panchal";
+for (const k in name) {
+	console.log(k);
 }
